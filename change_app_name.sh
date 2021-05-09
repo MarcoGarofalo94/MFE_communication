d14758185673a7e99722a6dcdb24c825f3359ecf
MarcoGarofalo94 <garofalomarco1994@libero.it>
2021-05-09 10:15:51 +0200

    fixes

--- change_app_name.sh
+++ change_app_name.sh
@@ -3,6 +3,11 @@ if [ -z $1 ]
 then
     echo "You should call the script passing the new name as shell script argument."
 else
-    find . \( -type d -name .git -prune \) -o -type f -exec  sed -i "s/customappname1/$1/g" {} +;
-    find . \( -type d -name .git -prune \) -o -type f -exec  sed -i "s/Customappname1/${1^}/g" {} +;
+    echo "changing id opbridge to $1 in all files..."
+    find . \( -type d -name .git -prune -name node_modules -prune \) -o -type f -exec  sed -i "s/customappname1/$1/g" {} +;
+    echo "changing id opbridge to $1 in all nginx.conf..."
+    find ../../../nginx/ \( -type d -name .git -prune -name node_modules -prune \) -o -type f -exec  sed -i "s/unimedocs/$1/g" {} +;
+    echo "changing namespace Opbridge to ${1^} in all files..."
+    find . \( -type d -name .git -prune -name node_modules -prune \) -o -type f -exec  sed -i "s/Customappname1/${1^}/g" {} +;
+    echo "Done."
 fi
